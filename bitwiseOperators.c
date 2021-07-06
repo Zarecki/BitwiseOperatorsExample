fd531b95d411295ae6920b13ccbebde9c4b22f9c
Zarecki <jonzarecki@gmail.com>
2021-07-06 16:05:56 +0100

    bitwise right shift example

--- bitwiseOperators.c
+++ bitwiseOperators.c
@@ -11,18 +11,21 @@ int main(){
     int xorResult = 0;
     int complementA = 0;
     int leftShiftedA = 0;
+    int rightShiftedA = 0;
 
     andResult = a & b; // 0000 1100
     orResult = a | b;  // 0011 1101
     xorResult = a ^ b; // 0011 0001
     complementA = ~a;  // 1100 0011
-    leftShiftedA = a << 2; // 1111 0000
+    leftShiftedA = a << 2;  // 1111 0000
+    rightShiftedA = a >> 2; // 0000 1111
 
     printf("60 'Bitwise And' 13\nresult is: %d\n\n", andResult);
     printf("60 'Bitwise Or' 13\nresult is: %d\n\n", orResult);
     printf("60 'Bitwise Exclusive Or' 13\nresult is: %d\n\n", xorResult);
     printf("'Bitwise Complement' 60\nresult is: %d\n\n", complementA);
-    printf("'Right shift' 60 by 2\nresult is: %d\n\n", leftShiftedA);
+    printf("'Left shift' 60 by 2\nresult is: %d\n\n", leftShiftedA);
+    printf("'Right shift' 60 by 2\nresult is: %d\n\n", rightShiftedA);
 
     return 0;
 }
