0eb1acef47826b713c9af3ad1667f2b1e45a0d10
Zarecki <jonzarecki@gmail.com>
2021-07-06 15:52:34 +0100

    bitwise or example

--- bitwiseOperators.c
+++ bitwiseOperators.c
@@ -7,10 +7,13 @@ int main(){
     unsigned int a = 60; // 0011 1100
     unsigned int b = 13; // 0000 1101
     int andResult = 0;
+    int orResult = 0;
 
-    andResult = a & b; //0000 1100
+    andResult = a & b; // 0000 1100
+    orResult = a | b;  // 0011 1101
 
     printf("60 'Bitwise And' 13\nresult is: %d\n\n", andResult);
+    printf("60 'Bitwise Or' 13\nresult is: %d\n\n", orResult);
 
     return 0;
 }
