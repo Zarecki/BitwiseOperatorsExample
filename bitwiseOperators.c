fea1f870bbe21924658380b6aa51a3964798918f
Zarecki <jonzarecki@gmail.com>
2021-07-06 15:54:52 +0100

    bitwise exclusive or example

--- bitwiseOperators.c
+++ bitwiseOperators.c
@@ -8,12 +8,15 @@ int main(){
     unsigned int b = 13; // 0000 1101
     int andResult = 0;
     int orResult = 0;
+    int xorResult = 0;
 
     andResult = a & b; // 0000 1100
     orResult = a | b;  // 0011 1101
+    xorResult = a ^ b; // 0011 0001
 
     printf("60 'Bitwise And' 13\nresult is: %d\n\n", andResult);
     printf("60 'Bitwise Or' 13\nresult is: %d\n\n", orResult);
+    printf("60 'Bitwise Exclusive Or' 13\nresult is: %d\n\n", xorResult);
 
     return 0;
 }
